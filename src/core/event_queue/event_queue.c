b734040a8d893b7b73c37d6647729bdc7e1f3aa3
kntzn <kntznart@gmail.com>
2026-06-01 15:11:54 +0300

    [Del]: event_queue_len

--- src/core/event_queue/event_queue.c
+++ src/core/event_queue/event_queue.c
@@ -2,7 +2,6 @@
 
 // ------- Private declarations ------ //
 static void     event_queue_clear ();
-static uint16_t event_queue_len   ();
 static bool     event_queue_pop (event_t* event);
 
 typedef struct
@@ -18,6 +17,7 @@ static event_queue_t queue =
 {
     .head = 0,
     .tail = 0,
+    .max_len_reached = 0,
     .buffer=queue_buffer
 };
 
@@ -26,6 +26,7 @@ static void default_handler(const event_t event)
 {
     // TODO: handle failed events
 }
+
 static event_handler_t event_handlers[__EVENT_MAX] = { };
 
 // ------- Public  defenitions ------- //
@@ -42,13 +43,16 @@ bool event_queue_register_event_handler (event_t event, event_handler_t handler)
 
 bool event_queue_push (event_t event)
 {
-    if (event_queue_len >= EVENT_QUEUE_SIZE)
+    uint16_t next = queue.tail + 1;
+    if (next >= EVENT_QUEUE_SIZE)
+        next = 0;
+    
+    if(next == queue.head)
         return false;
 
-    queue.buffer [queue.tail++] = event;
-    if (queue.tail >= EVENT_QUEUE_SIZE)
-        queue.tail = 0;
-    
+    queue.buffer [next] = event;
+    queue.tail = next;
+
     return true;
 }
 
@@ -101,13 +105,6 @@ static void event_queue_clear ()
 {
     queue.head = 0;
     queue.tail = 0;
-}
-
-static inline uint16_t event_queue_len ()
-{    
-    return queue.head > queue.tail ?
-           queue.head - queue.tail : 
-           EVENT_QUEUE_SIZE - queue.tail + queue.head;
 }
 
 static bool event_queue_pop (event_t* event)
